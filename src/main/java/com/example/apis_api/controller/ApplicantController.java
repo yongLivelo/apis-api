d341ca8bf861248c0141ea181133c221f88efa86
Julio Eduardo B. Livelo <94818606+yongLivelo@users.noreply.github.com>
2025-05-03 13:20:32 +0800

    remove import

--- src/main/java/com/example/apis_api/controller/ApplicantController.java
+++ src/main/java/com/example/apis_api/controller/ApplicantController.java
@@ -3,7 +3,6 @@ package com.example.apis_api.controller;
 import com.example.apis_api.model.Applicant;
 import com.example.apis_api.repo.ApplicantRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 

0893042f6aabf7911180b2a6dacf62af609d06b4
Livelo Julio B. <julioeduardolivelo@gmail.com>
2026-05-03 19:35:36 +0800

    fixed endpoints with appropriate output

--- src/main/java/com/apis/apis_api/controller/ApplicantController.java
+++ src/main/java/com/apis/apis_api/controller/ApplicantController.java
@@ -1,15 +1,15 @@
 package com.apis.apis_api.controller;
 
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import com.apis.apis_api.model.Applicant;
 import com.apis.apis_api.repo.ApplicantRepo;
 
 @RestController
+
 @RequestMapping("/api/applicants")
 public class ApplicantController {
     @Autowired
@@ -22,12 +22,29 @@ public class ApplicantController {
     }
 
     @PostMapping
-    public Applicant createApplicant(@RequestBody Applicant newApplicant) {
-        return applicantRepo.save(newApplicant);
+    public ResponseEntity<Applicant> createApplicant(@RequestBody Applicant newApplicant) {
+        return ResponseEntity.ok(applicantRepo.save(newApplicant));
     }
 
     @DeleteMapping("/{id}")
-    public void deleteApplicant(@PathVariable Long id) {
-        applicantRepo.deleteById(id);
+    public ResponseEntity<Void> deleteApplicant(@PathVariable Long id) {
+        return applicantRepo.findById(id).map(
+                applicant -> {
+                    applicantRepo.deleteById(id);
+                    return ResponseEntity.noContent().<Void>build();
+                }
+
+        ).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+
+    public ResponseEntity<Applicant> updateApplicant(@PathVariable Long id, @RequestBody Applicant updatedApplicant) {
+        return applicantRepo.findById(id)
+                .map(applicant -> {
+                    applicant.setFirstName(updatedApplicant.getFirstName());
+                    applicant.setLastName(updatedApplicant.getLastName());
+                    return ResponseEntity.ok(applicantRepo.save(applicant));
+                }).orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
